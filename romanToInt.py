e3fb60458258cfed874aac789d6536c5fce608a7
Andrew <terminatordog0000@gmail.com>
2023-03-10 14:54:08 -0500

    Finished direct algorithm

--- romanToInt.py
+++ romanToInt.py
@@ -1,1 +1,20 @@
-print("hello world")
+    
+def romanToInt(s):
+    vals = {'I':1, 'V':5, 'X':10,'L':50, 'C':100,'D':500,'M':1000}
+    sum = 0
+    i = 0
+    while i < len(s):
+        try:
+            if vals[s[i]] < vals[s[i+1]]:
+                sum += (vals[s[i+1]] - vals[s[i]])
+                i+=2
+                continue
+        except IndexError:
+            pass
+        sum += vals[s[i]]
+        i+=1
+    return sum
+
+if __name__ == "__main__":
+    s = "MCMXCIV"
+    print(romanToInt(s))
